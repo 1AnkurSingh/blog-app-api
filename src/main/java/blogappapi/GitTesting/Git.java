75ca45a4f2e59cdc44fbc9dd531bb4ddda56aa2a
Ankur Singh <ankurrana231002@gmail.com>
2023-06-07 12:22:23 +0530

    Git Test

---
+++ src/main/java/blogappapi/GitTesting/Git.java
@@ -0,0 +1,13 @@
+package blogappapi.GitTesting;
+
+public class Git {
+    public static void main(String[] args) {
+        int i=0;
+        while (i<=10){
+            System.out.println("a");
+            i++;
+        }
+
+        System.out.println();
+    }
+}
